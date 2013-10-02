228c62e7b7fb6235642b9f46801b5f0153ceb3d2
Emil Breding <emil.breding@gmail.com>
2013-10-02 14:23:52 +0200

    Added a threaded test

--- src/test/java/se/cygni/frequencylist/SimpleFrequencyListTest.java
+++ src/test/java/se/cygni/frequencylist/SimpleFrequencyListTest.java
@@ -15,4 +15,38 @@ public class SimpleFrequencyListTest {
         Assert.assertTrue(true);
     }
 
+    @Test
+    public void testThreadedAdds() throws Exception {
+        SimpleFrequencyList fList = new SimpleFrequencyList();
+
+
+        ExecutorService tp = Executors.newFixedThreadPool(3);
+        Runnable publisher1 = getRunner(fList, "1");
+        Runnable publisher11 = getRunner(fList, "1");
+        Runnable publisher2 = getRunner(fList, "2");
+
+        tp.execute(publisher1);
+        tp.execute(publisher11);
+        tp.execute(publisher2);
+
+        tp.shutdown();
+
+        Assert.assertTrue("Shutdown complete", tp.awaitTermination(1200, TimeUnit.MILLISECONDS));
+        Assert.assertEquals(100, fList.getFrequency("2"));
+        Assert.assertEquals(200, fList.getFrequency("1"));
+    }
+
+    private Runnable getRunner(final SimpleFrequencyList fList, final String number) {
+        return new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 100; i++) {
+                    try {
+                        fList.add(number);
+                        Thread.sleep(5);
+                    } catch (InterruptedException e) { }
+                }
+            }
+        };
+    }
 }
