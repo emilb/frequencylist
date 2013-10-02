fd818f9fadac78739094647933646e18d61bfb7b
Emil Breding <emil.breding@gmail.com>
2013-10-02 14:25:11 +0200

    Cleaned imports on test

--- src/test/java/se/cygni/frequencylist/SimpleFrequencyListTest.java
+++ src/test/java/se/cygni/frequencylist/SimpleFrequencyListTest.java
@@ -3,11 +3,6 @@ package se.cygni.frequencylist;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.List;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.TimeUnit;
-
 public class SimpleFrequencyListTest {
 
     @Test
